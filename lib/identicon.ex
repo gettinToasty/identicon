a1755002b937c0741aaa9228ba6c731a93a1eda6
gettinToasty <sean.r.beyer@gmail.com>
2017-12-06 23:53:36 -0800

    Build out the grid and determine color

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -6,6 +6,10 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> determine_color
+    |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
   end
 
   @doc """
@@ -13,10 +17,44 @@ defmodule Identicon do
 
   ## Examples
       iex> Identicon.hash_input("bob")
-      [159, 157, 81, 188, 112, 239, 33, 202, 92, 20, 243, 7, 152, 10, 41, 216]
+      %Identicon.Image{hex: [159, 157, 81, 188, 112, 239, 33, 202, 92, 20, 243, 7, 152, 10, 41, 216]}
   """
   def hash_input(input) do
-    :crypto.hash(:md5, input)
-    |> :binary.bin_to_list
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list
+
+    %Identicon.Image{ hex: hex }
+  end
+
+  def determine_color(%Identicon.Image{ hex: [r, g, b | _tail] } = image) do
+    %Identicon.Image{ image | color: { r, g, b } }
+  end
+
+  def build_grid(%Identicon.Image{ hex: hex } = image) do
+    grid =
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{ image | grid: grid }
+  end
+
+  def mirror_row([a, b | _tail] = row) do
+    row ++ [b, a]
+  end
+
+  def filter_odd_squares(%Identicon.Image{ grid: grid } = image) do
+    grid = Enum.filter grid, fn({ code, _index }) ->
+      rem(code, 2) == 0
+    end
+
+    %Identicon.Image{ image | grid: grid }
+  end
+
+  def build_pixel_map(%Identicon.Image{ grid: grid } = image) do
+    
   end
 end
