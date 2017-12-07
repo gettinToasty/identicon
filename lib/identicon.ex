66892d1d5f70fa66ab321a32e9d8aad84abc6337
gettinToasty <sean.r.beyer@gmail.com>
2017-12-06 22:32:33 -0800

    Add main pipeline and hashing method

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,22 @@
 defmodule Identicon do
   @moduledoc """
-  Documentation for Identicon.
+    A module that converts a string into a deterministic image.
   """
+
+  def main(input) do
+    input
+    |> hash_input
+  end
 
   @doc """
-  Hello world.
+  Converts a string input into an MD5 hash, returns an integer list representation
 
   ## Examples
-
-      iex> Identicon.hello
-      :world
-
+      iex> Identicon.hash_input("bob")
+      [159, 157, 81, 188, 112, 239, 33, 202, 92, 20, 243, 7, 152, 10, 41, 216]
   """
-  def hello do
-    :world
+  def hash_input(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
   end
 end
