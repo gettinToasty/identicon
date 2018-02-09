1f0ae0715125bac2c349922115e3db4dc803185b
Sean Beyer <sean.beyer@coverhound.com>
2018-02-09 14:29:02 -0800

    Add more docs to components

--- lib/image.ex
+++ lib/image.ex
@@ -1,3 +1,7 @@
 defmodule Identicon.Image do
+  @moduledoc """
+    A data structure that contains the necessary elements to create an identicon from a predetermined string.
+  """
+
   defstruct hex: nil, color: nil, grid: nil, pixel_map: nil
 end
--- mix.exs
+++ mix.exs
@@ -28,6 +28,10 @@ defmodule Identicon.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:egd, github: "erlang/egd"},
+      {:earmark, "~> 0.1", only: :dev},
+      {:ex_doc, "~> 0.11", only: :dev},
+    ]
   end
 end
